cac90c1a3088dd45dc19b2302aa91a7cb7bd4bfd
Hua <75751972+huaj1ng@users.noreply.github.com>
2025-01-29 23:38:37 +0100

    Fix file grid sorting of size
    ERM38961

--- src/Data/FileQueryStore/PrimaryDataProvider.php
+++ src/Data/FileQueryStore/PrimaryDataProvider.php
@@ -12,7 +12,8 @@ class PrimaryDataProvider extends TitlePrimaryDataProvider {
 
 	private $dbFieldMapping = [
 		'timestamp' => 'img_timestamp',
-		'title' => 'mti_title'
+		'title' => 'mti_title',
+		'file_size' => 'img_size'
 	];
 
 	/**
@@ -114,7 +115,8 @@ class PrimaryDataProvider extends TitlePrimaryDataProvider {
 		return array_merge(
 			parent::getFields(), [
 				'img_actor', 'img_major_mime', 'img_minor_mime', 'actor_name',
-				'comment_text', "GROUP_CONCAT( cl_to SEPARATOR '|') categories", 'img_timestamp'
+				'comment_text', "GROUP_CONCAT( cl_to SEPARATOR '|') categories",
+				'img_timestamp', 'img_size'
 			] );
 	}
 
@@ -144,6 +146,7 @@ class PrimaryDataProvider extends TitlePrimaryDataProvider {
 			TitleRecord::IS_CONTENT_PAGE => in_array( $row->page_namespace, $this->contentNamespaces ),
 			FileRecord::FILE_TIMESTAMP => $row->img_timestamp,
 			FileRecord::FILE_EXTENSION => $this->getExtension( $row->page_title ),
+			FileRecord::FILE_SIZE => $row->img_size,
 			FileRecord::MIME_MAJOR => $row->img_major_mime,
 			FileRecord::MIME_MINOR => $row->img_minor_mime,
 			FileRecord::FILE_AUTHOR_ID => $row->img_actor,
