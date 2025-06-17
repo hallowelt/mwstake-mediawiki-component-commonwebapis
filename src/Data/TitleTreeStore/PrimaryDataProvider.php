ce0e3dffe920f39ad82131f92d0f2c3d3494525e
d.savuljesku <d.savuljesku@gmail.com>
2025-06-17 11:00:47 +0200

    CI fixes

--- src/Data/TitleTreeStore/PrimaryDataProvider.php
+++ src/Data/TitleTreeStore/PrimaryDataProvider.php
@@ -14,18 +14,17 @@ class PrimaryDataProvider extends \MWStake\MediaWiki\Component\CommonWebAPIs\Dat
 	/** @var array|null */
 	private $expandPaths = null;
 
-	/** @var NamespaceInfo */
-	private $nsInfo;
-
   /** @var PermissionManager */
 	private $permissionManager;
 
 	/**
 	 * @inheritDoc
 	 */
-	public function __construct( IDatabase $db, Schema $schema, Language $language, NamespaceInfo $nsInfo, PermissionManager $permissionManager ) {
+	public function __construct(
+		IDatabase $db, Schema $schema, Language $language, NamespaceInfo $nsInfo,
+		PermissionManager $permissionManager
+	) {
 		parent::__construct( $db, $schema, $language, $nsInfo );
-		$this->nsInfo = $nsInfo;
 		$this->permissionManager = $permissionManager;
 	}
 
